ec1a80a55aaff642e6a76427fc70c63ca4eeb312
Ajay Das <AjayDas2003@gmail.com>
2025-07-22 19:56:16 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='"AWS s3 bucket for data versioning"',
+    author='Ajay',
+    license='MIT',
+)
